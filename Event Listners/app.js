ee8008f8b62e3577b0325069cb93f653e36d2723
Parth <parthfshah@gmail.com>
2022-05-19 16:03:00 -0400

    Event Listeners

--- Event Listners/app.js
+++ Event Listners/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
@@ -14,6 +15,12 @@ const app = Vue.createApp({
     },
     setName(event) {
       this.name = event.target.value;
+    },
+    submitForm() {
+      alert("submitted");
+    },
+    confirmName() {
+      this.confirmedName = this.name;
     },
   },
 });
